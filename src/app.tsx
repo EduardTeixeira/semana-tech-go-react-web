7d7ae0a7664db5e0bc7b7377012addeec524b517
Eduard Teixeira <eduardteixeira01@gmail.com>
2024-09-13 16:03:43 -0300

    conectando com o banco de dados e exemplo de utilizacao de websocket

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,8 @@ import { Toaster } from 'sonner';
 
 import { CreateRoom } from './pages/create-room';
 import { Room } from './pages/room';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './lib/react-query';
 
 const router = createBrowserRouter(
   [
@@ -19,9 +21,9 @@ const router = createBrowserRouter(
 
 export function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <Toaster invert richColors />
-    </>
+    </QueryClientProvider>
   )
 }
--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,9 +1,10 @@
-import { ArrowRight, Share2 } from 'lucide-react';
+import { Share2 } from 'lucide-react';
+import { Suspense } from 'react';
 import { useParams } from 'react-router-dom';
 import { toast } from 'sonner';
 
 import amaLogo from '../assets/ama-logo.svg';
-import { Message } from '../components/message';
+import { Messages } from '../components/messages';
 
 export function Room() {
 
@@ -45,32 +46,9 @@ export function Room() {
 
       <div className='h-px w-full bg-zinc-900'></div>
 
-      <form
-        className='flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1'>
-
-        <input
-          type='text'
-          name="theme"
-          placeholder='Qual a sua pergunta?'
-          autoComplete='off'
-          className='flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-100 placeholder:text-zinc-500'
-        />
-
-        <button
-          type='submit'
-          className='bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-orange-500'
-        >
-          Criar Pergunta
-          <ArrowRight className='size-4' />
-        </button>
-
-      </form>
-
-      <ol className='list-decimal list-outside px-3 space-y-8'>
-        <Message text='Apenas uma mensagem de teste para o texto!' amountOfReactions={123} answered />
-        <Message text='Outra mensagem de teste' amountOfReactions={120} />
-        <Message text='Mensagem do teste final do texto...' amountOfReactions={23} />
-      </ol>
+      <Suspense fallback={<p>Carregando...</p>}>
+        <Messages />
+      </Suspense>
 
     </div>
   )
